57caf8946d933d04191b36bb6e70a0f1c322ee43
saidaraok <saidaraoinnet@gmail.com>
2025-01-03 00:01:22 +0530

    1st commit

---
+++ stage1/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main ()
+{
+	printf("HelloWorld\r\n");
+	printf("HelloWorld\r\n");
+	printf("HelloWorld\r\n");
+	printf("HelloWorld\r\n");
+	printf("HelloWorld\r\n");
+	return 0;
+}
